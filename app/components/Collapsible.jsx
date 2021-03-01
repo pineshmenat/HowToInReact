c2e39308a15a9e2f5ea7f956130c68e7561d36c9
pineshmenat <pineshmenat@gmail.com>
2021-02-28 19:04:06 -0500

    add Styled Component library

--- app/components/Collapsible.jsx
+++ app/components/Collapsible.jsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import {FaAngleDown, FaAngleRight} from 'react-icons/fa'
-export default class Collapsible extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            toggle: false
-        }
+import React, { useState } from "react";
+import { FaAngleDown, FaAngleRight } from "react-icons/fa";
+import styled from "styled-components";
+
+const CollapsibleDiv = styled.div`
+    width: 100%;
+`;
+
+const CollapsibleDivContent = styled.div`    
+    padding: 16px 16px;
+    overflow: hidden;
+    background-color: #f1f1f1;
+`;
+
+const Button = styled.button`
+    background-color: #777;
+    color: white;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+    &:hover {
+        background-color: #555;
     }
-    render() {
-        console.log(this.state.toggle);
-        return(
-            <div className="collapsible">
-                <button type="button" className="btn" onClick={() => this.setState((prevState) => ({toggle: !prevState.toggle}))}>
-                    {this.state.toggle ? "Close": "Open"} Collapsible
-                    {this.state.toggle ? <FaAngleDown className="fa" size={16}/> : <FaAngleRight className="fa" size={16}/>}
-                </button>
-                {this.state.toggle && <div className="content">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>}
-            </div>
-        )
-    }
+`
+
+export default function Collapsible() {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <CollapsibleDiv>
+      <Button
+        type="button"
+        onClick={() => setToggle(!toggle)}
+      >
+        {toggle ? "Close" : "Open"} Collapsible
+        {toggle ? (
+          <FaAngleDown className="fa" size={16} style={{float: "right"}} />
+        ) : (
+          <FaAngleRight className="fa" size={16} style={{float: "right"}} />
+        )}
+      </Button>
+      {toggle && (                                                                                              
+        <CollapsibleDivContent>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum. Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </CollapsibleDivContent>
+      )}
+    </CollapsibleDiv>
+  );
 }
--- app/index.js
+++ app/index.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from "styled-components";
 import Collapsible from './components/Collapsible'
 import './index.css'
+
+const Container = styled.div`
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: nowrap;
+    width: 80%;
+    padding: 20px;
+`;
 
 export default class App extends React.Component {
     render() {
         return(
-            <div className="container">
+            <Container>
                 <Collapsible/>
-            </div>
+            </Container>
         )
     }
 }
